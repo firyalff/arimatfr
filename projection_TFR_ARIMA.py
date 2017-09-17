f791fa7aa9d0c5ae51683eb57694688334552e6b
firyal <firyal.fakhril@gmail.com>
2017-09-17 14:10:30 +0700

    prediction result saved to csv

--- projection_TFR_ARIMA.py
+++ projection_TFR_ARIMA.py
@@ -2,28 +2,38 @@ from pandas import read_csv
 from matplotlib import pyplot
 from statsmodels.tsa.arima_model import ARIMA
 from sklearn.metrics import mean_squared_error
+import csv
+import time
 
 series = read_csv('./data/spain_fertility_TFR.csv', header=0, parse_dates=[0], index_col=0, squeeze=True)
 
 X = series.values
 
-print(X)
-
-size = int(len(X) * 0.66)
+size = int(len(X) * 0.5)
 train, test = X[0:size], X[size:len(X)]
+
+finalpredictions= []
 
 history = [x for x in train]
 predictions = list()
 for t in range(len(test)):
-	model = ARIMA(history, order=(2,1,0))
-	model_fit = model.fit(disp=0)
-	output = model_fit.forecast()
-	yhat = output[0]
-	predictions.append(yhat)
-	obs = test[t]
-	history.append(obs)
-	print('predicted=%f, expected=%f' % (yhat, obs))
+    model = ARIMA(history, order=(2,1,0))
+    model_fit = model.fit(disp=0)
+    output = model_fit.forecast()
+    yhat = output[0]
+    predictions.append(yhat)
+    obs = test[t]
     
+    history.append(obs)
+    finalpredictions.append([obs, yhat[0]])
+    print('predicted=%f, expected=%f' % (yhat, obs))
+    
+with open("%s-prediction.csv" % (time.time()), 'w') as csvfile:
+    filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    filewriter.writerow(['Expected', 'Predictions'])
+    for predictiondata in finalpredictions:
+        filewriter.writerow(predictiondata)
+
 error = mean_squared_error(test, predictions)
 
 print('Test MSE: %.3f' % error)
